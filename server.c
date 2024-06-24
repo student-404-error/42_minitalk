5b1020a2aa35d513078cf66880bcefc9bcfefd43
student-404-error <ksh30102@gmail.com>
2024-06-25 00:04:14 +0200

    Create server

--- server.c
+++ server.c
@@ -2,31 +2,43 @@
 #include <unistd.h>
 #include <signal.h>
 #include "libft.h"
+#include "ft_printf.h"
 
-#define UTF8_1 0x7F
-#define UTF8_2 0x7FF
-#define UTF8_3 0xFFFF
-#define UTF8_4 0x1FFFFF
-
-void	get_str(int sig)
+void	get_str(int sig, siginfo_t *info, void *ucontext)
 {
-	char	chr;
+	static char	chr;
+	static int	bit;
 
-	chr = sig;
-	ft_putnbr_fd(sig, 1);
-	write(1, "\n", 1);
-	write(1, &chr, 1);
+	(void) ucontext;
+	(void) info;
+	if (sig == SIGUSR1)
+		chr |= 0;
+	else if (sig == SIGUSR2)
+		chr |= 1;
+	if (bit < 7)
+		chr <<= 1;
+	bit++;
+	if (bit == 8)
+	{
+		write(1, &chr, 1);
+		bit = 0;
+		chr = 0;
+	}
 }
 
 int	main(void)
 {
+	struct sigaction act;
+	ft_memset(&act, 0, sizeof(act));
+	act.sa_sigaction =  get_str;
+	act.sa_flags = SA_SIGINFO;
+	sigemptyset(&act.sa_mask);
+	
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
 	ft_putstr_fd("pid: ", 1);
 	ft_putnbr_fd(getpid(), 1);
 	ft_putchar_fd('\n', 1);
-	unsigned char	chr = ;
-	printf("%d", chr);
-	signal(SIGUSR1, get_str);
-	signal(SIGUSR2, get_str);
 	while (1)
 	{
 		pause();
